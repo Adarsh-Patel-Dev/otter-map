3756f6c72e742c1d6b1196770f7f753122eb442f
Adarsh2k21 <adarsh.patel9619@gmail.com>
2022-08-07 15:24:37 +0530

    Added map

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,33 +1,24 @@
 import React from "react";
 import { useUserContext } from "../../userContext";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
 import "./map.css"
 
+import {layer,Map,Layers} from "react-openlayers"
+
 function Maps() {
-  new Map({
-    target: "map",
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        }),
-      }),
-    ],
-    view: new View({
-      center: [0, 0],
-      zoom: 2,
-    }),
-  });
+  
 
   const { name } = useUserContext();
   return (
     <section>
       <h2 className="header">Welcome <em>{name}</em></h2>
       <div className="map-container">
-        This is map section.
+        <Map view={{
+          center:[0,0],zoom:1
+        }}>
+          <Layers>
+            <layer.Tile></layer.Tile>
+          </Layers>
+        </Map>
       </div>
     </section>
   );
